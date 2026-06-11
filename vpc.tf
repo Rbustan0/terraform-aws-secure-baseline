1acd3f633400adc363f9aad126bce2081b70c4fa
rbustan0 <royebustan@gmail.com>
2026-06-11 13:12:30 +0300

    add public and private subnets in eu-north-1a

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,27 @@ resource "aws_vpc" "main" {
         Project = "terraform-aws-secure-baseline"
     }
 }
+
+resource "aws_subnet" "public" {
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = "10.0.1.0/24"
+    availability_zone       = "eu-north-1a"
+    map_public_ip_on_launch = false
+
+    tags = {
+        Name    = "secure-baseline-public"
+        Project = "terraform-aws-secure-baseline"
+    }
+}
+
+resource "aws_subnet" "private" {
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = "10.0.2.0/24"
+    availability_zone       = "eu-north-1a"
+    map_public_ip_on_launch = false
+
+    tags = {
+        Name    = "secure-baseline-private"
+        Project = "terraform-aws-secure-baseline"
+    }
+}
